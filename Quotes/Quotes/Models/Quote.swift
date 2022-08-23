4f8c05886f5c6e7b83061a3421264684d36578c8
Veer Singh <veersingh209@icloud.com>
2022-08-23 13:38:14 -0700

    Quote Model

---
+++ Quotes/Quotes/Models/Quote.swift
@@ -0,0 +1,18 @@
+//
+//  Quote.swift
+//  Quotes
+//
+//  Created by Veer Singh on 8/23/22.
+//
+
+import Foundation
+
+class Quote: Identifiable, Decodable {
+    
+    var id:UUID?
+    
+    var quote: String
+    var author: String
+    var details: String
+    
+}
