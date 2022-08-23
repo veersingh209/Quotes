196f7a50cbf67498519d6c972a974055ba559945
Veer Singh <veersingh209@icloud.com>
2022-08-23 13:32:02 -0700

    Xcode init

---
+++ Quotes/Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Veer Singh on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
