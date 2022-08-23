2aa6a958c3c765632e463b89de4a66e8f4f91f8c
Veer Singh <veersingh209@icloud.com>
2022-08-23 13:46:44 -0700

    Get data from JSON file

---
+++ Quotes/Quotes/Service/DateService.swift
@@ -0,0 +1,48 @@
+//
+//  DateService.swift
+//  Quotes
+//
+//  Created by Veer Singh on 8/23/22.
+//
+
+import Foundation
+
+class DataService {
+    
+    static func getQuoteData() -> [Quote]{
+        
+        let stringPathToDataFile = Bundle.main.path(forResource: "data", ofType: "json")
+        
+        guard let stringURL = stringPathToDataFile else {
+            return [Quote]()
+        }
+        
+        let url = URL(fileURLWithPath: stringURL)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let quoteData = try decoder.decode([Quote].self, from: data)
+                
+                for quote in quoteData {
+                    quote.id = UUID()
+                }
+                
+                return quoteData
+                
+            } catch {
+                print("ERROR: Unable to decode data. Error: \(error)")
+            }
+            
+            
+        } catch {
+            print("ERROR: Unable to get data from content. \n \(error)")
+        }
+        print("ERROR: Unable to find data file")
+        return [Quote]()
+    }
+    
+}
